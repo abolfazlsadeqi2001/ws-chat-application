8688a7be597bbf67e63a917cbd0374b759d87360
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-21 17:19:57 +0430

    implements chat.html

--- chat-application/src/main/java/main/controllers/authentication/AuthenticatorController.java
+++ chat-application/src/main/java/main/controllers/authentication/AuthenticatorController.java
@@ -1,24 +1,20 @@
 package main.controllers.authentication;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthenticatorController {
-	// TODO using session instead of cookies
-	private final int COOKIES_LIFE_TIME = 3 * 24 * 60 * 60;
 	public final String NAME_PARAM = "name";
+	public final String CHAT_PATH = "chat";
 	
 	@RequestMapping("/login")
-	public void login(@RequestParam(name=NAME_PARAM,required=true) String name,
-			HttpServletResponse res) {
-		Cookie nameCookie = new Cookie(NAME_PARAM,name);
-		nameCookie.setMaxAge(COOKIES_LIFE_TIME);
-		res.addCookie(nameCookie);
+	public String login(@RequestParam(name=NAME_PARAM,required=true) String name,
+			Model model) {
+		model.addAttribute(NAME_PARAM, name);
+		return CHAT_PATH;
 	}
 	
 }
